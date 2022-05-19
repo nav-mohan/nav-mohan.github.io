6d1dad3868ae434fee5634247f13e6be70ae360b
nav-mohan <navaneeth.mohan94@gmail.com>
2022-05-18 23:29:16 -0400

    css styling done

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
 posts = document.getElementById("posts");
 
-fetch('posts.json')
-.then(function(res){
-    console.log(res);
-    if(res.status!==200){
-        console.log('error');
-    }
-    return (res.json())
-})
-.then(function(post_details){
-    for (let i = 0; i < post_details['posts'].length; i++) {
-        const post = post_details['posts'][i];
-        post_div = document.createElement('div');
-        post_img = document.createElement('img');
-        post_img.src = post['img']
-        post_anchor = document.createElement('a');
-        post_anchor.href = post['link']
-        post_anchor.innerText = post['title']
+const loadData = function(){
+    fetch('posts.json')
+    .then(function(res){
+        console.log(res);
+        if(res.status!==200){
+            console.log('error');
+        }
+        return (res.json())
+    })
+    .then(function(post_details){
+        for (let i = 0; i < post_details['posts'].length; i++) {
+            const post = post_details['posts'][i];
+            post_div = document.createElement('div');
+            post_desc = document.createElement('div');
+            post_desc.innerHTML = '<h3>' + post['desc'] + '</h3>';
+            post_desc.classList.add('description');
+            post_img = document.createElement('img');
+            post_img.src = post['img']
+            post_anchor = document.createElement('a');
+            post_anchor.href = post['link']
+            post_anchor.innerText = post['title']
 
-        post_div.appendChild(post_anchor)
-        post_div.appendChild(post_img)
+            post_div.appendChild(post_img)
+            post_div.appendChild(post_desc)
+            post_div.appendChild(post_anchor)
 
-        post_div.classList.add('post');
-        posts.appendChild(post_div)
+            post_div.classList.add('post');
+            posts.appendChild(post_div)
 
-    }
-})
+        }
+    })
+}
+
+window.addEventListener('load',loadData);
